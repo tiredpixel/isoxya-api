d3f22da9362e723aa00e6d1315468703848ffb6c
Nic Williams <tp@tiredpixel.com>
2022-05-15 18:17:30 +0200

    bin: fix init ports; add x-exports.sh

---
+++ bin/x-exports.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -eu
+set -o pipefail
+#-------------------------------------------------------------------------------
+export ISOXYA_DEBUG=0
+export PATH=$HOME/code/isoxya/api-pro/bin:$HOME/code/isoxya/api/bin/pro:$HOME/code/isoxya/api/bin:$PATH
